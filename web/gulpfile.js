2a96de7ae3c264dd44f63f1ebc1caf856f313582
Chris Shorrock <chris.shorrock@gmail.com>
2015-04-30 03:54:42 +0000

    got all the web dev setup with proper watching whil in dev mode, consoladated gulp file

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,10 +1,3 @@
+'use strict';
 require("coffee-script/register")
-
-var gulp   = require('gulp');
-var fs     = require('fs');
-var path   = require('path');
-var tasks  = fs.readdirSync('./tasks/').filter(function(name) { return /(\.(js|coffee)$)/i.test(path.extname(name))} );
-
-tasks.forEach(function(task) {
-	require('./tasks/' + task);
-});
+require("./gulpfile.coffee")
--- web/tasks/less.coffee
+++
@@ -1,9 +0,0 @@
-gulp     = require "gulp"
-less     = require "gulp-less"
-settings = require "./settings"
-
-gulp.task 'less', () ->
-  gulp
-    .src(settings.stylesheets.src)
-    .pipe(less())
-    .pipe(gulp.dest(settings.stylesheets.dest))
